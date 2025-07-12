ee90a5df1c22c701b4b679a3eb397284a6bd07f7
Fevzi Atanoğlu <95905332+fevziatanoglu@users.noreply.github.com>
2025-07-12 19:21:29 +0300

    feat: update App component to implement routing with new pages for Landing, Dashboard, PublicMenu, and Tables

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import { Orders } from "./components/Orders/Orders";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/Landing/LandingPage";
+import { Dashboard } from "./components/Dashboard";
 import { Menu } from "./components/Menu/Menu";
+import { Orders } from "./components/Orders/Orders";
+import { PublicMenu } from "./components/PublicMenu/PublicMenu";
+import { Tables } from "./components/Tables/Tables";
 
 
 
 function App() {
   return (
     <Router>
-      <Menu />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/publicmenu" element={<PublicMenu />} />
+        <Route path="/tables" element={<Tables />} />
+      </Routes>
     </Router>
   );
 }
