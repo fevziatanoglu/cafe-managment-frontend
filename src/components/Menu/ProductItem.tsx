d66aa0015705fbeb4187b2341dca567d401f380f
Fevzi Atanoğlu <95905332+fevziatanoglu@users.noreply.github.com>
2025-08-14 16:06:04 +0300

    feat: add user role check for admin actions in ProductItem component

--- src/components/Menu/ProductItem.tsx
+++ src/components/Menu/ProductItem.tsx
@@ -24,26 +24,31 @@ const getCategoryColor = (category?: PRODUCT_CATEGORY) => {
 
 function ProductItem({ product }: ProductItemProps) {
 
-  const { openModal} = useStore();
- 
+  const { openModal, user } = useStore();
+
   return (
     <div className="bg-white rounded-xl shadow-md p-4 flex flex-row border border-amber-100 hover:shadow-lg transition-all duration-200 relative">
       {/* Top right: Edit & Delete buttons */}
-      <div className="absolute top-3 right-3 flex space-x-2 z-10">
-        <button
-          onClick={() => openModal(<ProductForm product={product} /> , "Update Product")}
-          className="p-2 rounded-lg bg-amber-100 hover:bg-amber-200 text-amber-700 transition"
-          title="Edit"
-        >
-          <Edit2 className="h-4 w-4" />
-        </button>
-        <button
-          className="p-2 rounded-lg bg-red-100 hover:bg-red-200 text-red-700 transition"
-          title="Delete"
-        >
-          <Trash2 className="h-4 w-4" />
-        </button>
-      </div>
+      {
+        user?.role === 'admin' && (
+          <div className="absolute top-3 right-3 flex space-x-2 z-10">
+            <button
+              onClick={() => openModal(<ProductForm product={product} />, "Update Product")}
+              className="p-2 rounded-lg bg-amber-100 hover:bg-amber-200 text-amber-700 transition"
+              title="Edit"
+            >
+              <Edit2 className="h-4 w-4" />
+            </button>
+            <button
+              className="p-2 rounded-lg bg-red-100 hover:bg-red-200 text-red-700 transition"
+              title="Delete"
+            >
+              <Trash2 className="h-4 w-4" />
+            </button>
+          </div>
+        )
+      }
+
       {/* Left: Image/Icon */}
       <div className="flex-shrink-0 flex items-center justify-center w-32 h-32 bg-gradient-to-r from-orange-500 to-red-600 rounded-xl overflow-hidden">
         {product.image ? (
